c9c7083d186aa0ec241383c59f803f3f9efa01de
Gobiner <Gobiner@localhost>
2009-07-28 22:49:33 -0700

    First bits of minimize to tray functionality.

--- War3Macro/MainWindow.xaml.cs
+++ War3Macro/MainWindow.xaml.cs
@@ -22,13 +22,42 @@ namespace War3Macro
     public partial class MainWindow : Window
     {
 		private KeyboardHook hotkeyregistration;
+
 		private System.Windows.Forms.NotifyIcon TippuTrayNotify;
 		private System.Windows.Forms.ContextMenuStrip ctxTrayMenu;
 		private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private bool isAppExiting = false; 
 
         public MainWindow()
         {
             InitializeComponent();
+
+            //this.Icon = BitmapFrame.Create(new Uri("app.bmp", UriKind.Relative));
+
+            //Create an instance of the NotifyIcon Class
+            TippuTrayNotify = new System.Windows.Forms.NotifyIcon();
+
+            // This icon file needs to be in the bin folder of the application
+            //TippuTrayNotify.Icon = new System.Drawing.Icon("Application.ico");
+
+            //show the Tray Notify Icon
+            TippuTrayNotify.Visible = true;
+
+            //Create a object for the context menu
+            ctxTrayMenu = new System.Windows.Forms.ContextMenuStrip();
+
+            //Add the Menu Item to the context menu
+            System.Windows.Forms.ToolStripMenuItem mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            mnuExit.Text = "Exit";
+            mnuExit.Click += new EventHandler(mnuExit_Click);
+            ctxTrayMenu.Items.Add(mnuExit);
+
+            //Add the Context menu to the Notify Icon Object
+            TippuTrayNotify.ContextMenuStrip = ctxTrayMenu;
+
+            IsVisibleChanged += new DependencyPropertyChangedEventHandler(MainWindow_IsVisibleChanged);
+            StateChanged += new EventHandler(MainWindow_StateChanged);
+
 
             foreach (var something in tabControl1.Items)
             {
@@ -43,9 +72,37 @@ namespace War3Macro
 			hotkeyregistration.KeyDown += KeyPressHandler;
         }
 
+        void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                TippuTrayNotify.ShowBalloonTip(2000);
+            }
+        }
+
+        void MainWindow_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            TippuTrayNotify.Visible = !(bool)e.NewValue;
+        }
+
+        void mnuExit_Click(object sender, EventArgs e)
+        {
+            isAppExiting = true;
+            this.Close();
+            TippuTrayNotify.Visible = false;
+        } 
+
 		void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
 			Properties.Settings.Default.Save();
+            this.Hide();
+            TippuTrayNotify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            TippuTrayNotify.BalloonTipTitle = "Tippu Tray Notify";
+            TippuTrayNotify.BalloonTipText = "Tippu Tray Notify has been minimized to the system tray. To open the application, double-click the icon in the system tray.";
+            TippuTrayNotify.ShowBalloonTip(400);
+
+
 		}
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
