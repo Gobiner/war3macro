75cab40a7cd6791c7b42cc6d592fd363920ed2d9
Gobiner <Gobiner@localhost>
2009-08-10 20:52:19 -0700

    Fucking FINALLY. Shit works. Thanks BillG.

--- War3Macro/Send Input Wrapper.cs
+++ War3Macro/Send Input Wrapper.cs
@@ -6,361 +6,166 @@ using System.Runtime.InteropServices;
 
 namespace War3Macro
 {
-	public static class SendInputWrapper
-	{
+    public static class SendInputWrapper
+    {
 
-		public static void SendString(string inp, bool toAll)
-		{
-			IEnumerable<INPUT> inputs = new INPUT[0];
-			if (toAll)
-				inputs = inputs.Concat(new INPUT[2] { LShiftUpInput(), LShiftDownInput() });
-			inputs = inputs.Concat(new INPUT[2] { EnterDownInput(), EnterUpInput() });
-			if (toAll)
-				inputs = inputs.Concat(new INPUT[1] { LShiftUpInput() });
-			inputs = inputs.Concat(BuildInputsForString(inp));
-			inputs = inputs.Concat(new INPUT[2] { EnterDownInput(), EnterUpInput() });
-			var finalinputs = inputs.ToArray();
-			var sent = SendInput((uint)finalinputs.Length, finalinputs, Marshal.SizeOf(finalinputs[0]));
-			var error = "0x" + ((uint)Marshal.GetLastWin32Error() | (uint)0x80000000).ToString("X");
+        public static void SendString(string inp, bool toAll)
+        {
+            IEnumerable<INPUT> inputs = new INPUT[0];
+            if (toAll)
+                inputs = inputs.Concat(new INPUT[2] { LShiftUpInput, LShiftDownInput });
+            inputs = inputs.Concat(new INPUT[2] { EnterDownInput, EnterUpInput });
+            if (toAll)
+                inputs = inputs.Concat(new INPUT[1] { LShiftUpInput });
+            inputs = inputs.Concat(BuildInputsForString(inp));
+            inputs = inputs.Concat(new INPUT[2] { EnterDownInput, EnterUpInput });
+            var finalinputs = inputs.ToArray();
 
-			if (sent != finalinputs.Length)
-			{
-				var noop = "";
-			}
-		}
+            var sent = SendInput((uint)finalinputs.Length, finalinputs, Marshal.SizeOf(finalinputs[0]));
+            var error = "0x" + ((uint)Marshal.GetLastWin32Error() | (uint)0x80000000).ToString("X");
 
-		private static IEnumerable<INPUT> BuildInputsForString(string inp)
-		{
-			if (string.IsNullOrEmpty(inp))
-				yield break;
-			foreach (char chr in inp)
-			{
-				if (char.IsUpper(chr))
-				{
-					yield return LShiftDownInput();
-				}
-				yield return CharDownInput(chr);
-				yield return CharUpInput(chr);
-				if (char.IsUpper(chr))
-				{
-					yield return LShiftUpInput();
-				}
-			}
-		}
-		private static INPUT LShiftDownInput()
-		{
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = (ushort)VK.LSHIFT;
-			ret.ki.dwFlags = 0;
-			return ret;
-		}
-		private static INPUT LShiftUpInput()
-		{
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = (ushort)VK.LSHIFT;
-			ret.ki.dwFlags = 2;
-			return ret;
-		}
-		private static INPUT CharDownInput(char chr)
-		{
-			byte vk = VkKeyScan(chr);
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = vk;
-			ret.ki.dwFlags = 0;
-			return ret;
-		}
-		private static INPUT CharUpInput(char chr)
-		{
-			byte vk = VkKeyScan(chr);
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = vk;
-			ret.ki.dwFlags = 2;
-			return ret;
-		}
-		private static INPUT EnterDownInput()
-		{
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = (ushort)VK.RETURN;
-			ret.ki.dwFlags = 0;
-			return ret;
-		}
-		private static INPUT EnterUpInput()
-		{
-			var ret = new INPUT();
-			ret.type = INPUT_TYPE.KEYBOARD;
-			ret.ki = new KEYBOARD_INPUT();
-			ret.ki.wVk = (ushort)VK.RETURN;
-			ret.ki.dwFlags = 2;
-			return ret;
-		}
+            //var exception = Marshal.GetExceptionForHR((int)((uint)Marshal.GetLastWin32Error() | (uint)0x80000000));
+            var exception = Marshal.GetExceptionForHR(Marshal.GetLastWin32Error());
+            if (sent != finalinputs.Length)
+            {
+                var noop = "";
+            }
+        }
 
+        private static IEnumerable<INPUT> BuildInputsForString(string inp)
+        {
+            if (string.IsNullOrEmpty(inp))
+                yield break;
+            foreach (char chr in inp)
+            {
+                yield return CharDownInput(chr);
+                yield return CharUpInput(chr);
+            }
+        }
+        private static INPUT LShiftDownInput
+        {
+            get
+            {
+                var ret = new INPUT();
+                ret.type = INPUT_KEYBOARD;
+                ret.mkhi.ki = new KEYBDINPUT();
+                ret.mkhi.ki.wVk = 0x10;
+                ret.mkhi.ki.dwFlags = 0;
+                return ret;
+            }
+        }
+        private static INPUT LShiftUpInput
+        {
+            get
+            {
+                var ret = new INPUT();
+                ret.type = INPUT_KEYBOARD;
+                ret.mkhi.ki = new KEYBDINPUT();
+                ret.mkhi.ki.wVk = 0x10;
+                ret.mkhi.ki.dwFlags = KEYEVENTF_KEYUP;
+                return ret;
+            }
+        }
+        private static INPUT CharDownInput(char chr)
+        {
+            var ret = new INPUT();
+            ret.type = INPUT_KEYBOARD;
+            ret.mkhi.ki = new KEYBDINPUT();
+            ret.mkhi.ki.wVk = 0;
+            ret.mkhi.ki.wScan = chr;
+            ret.mkhi.ki.dwFlags = KEYEVENTF_UNICODE;
+            return ret;
+        }
+        private static INPUT CharUpInput(char chr)
+        {
+            var ret = new INPUT();
+            ret.type = INPUT_KEYBOARD;
+            ret.mkhi.ki = new KEYBDINPUT();
+            ret.mkhi.ki.wVk = 0;
+            ret.mkhi.ki.wScan = chr;
+            ret.mkhi.ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_UNICODE;
+            return ret;
+        }
+        private static INPUT EnterDownInput
+        {
+            get
+            {
+                var ret = new INPUT();
+                ret.type = INPUT_KEYBOARD; 
+                ret.mkhi.ki = new KEYBDINPUT();
+                ret.mkhi.ki.wVk = 0x0D;
+                ret.mkhi.ki.dwFlags = 0;
+                return ret;
+            }
+        }
+        private static INPUT EnterUpInput
+        {
+            get
+            {
+                var ret = new INPUT();
+                ret.type = INPUT_KEYBOARD;
+                ret.mkhi.ki = new KEYBDINPUT();
+                ret.mkhi.ki.wVk = 0x0D;
+                ret.mkhi.ki.dwFlags = KEYEVENTF_KEYUP;
+                return ret;
+            }
+        }
 
-		[DllImport("user32.dll")]
-		public static extern byte VkKeyScan(char ch);
-		[DllImport("user32.dll")]
-		public static extern uint MapVirtualKey(uint uCode, uint uMapType);
-		[DllImport("user32.dll", SetLastError = true)]
-		static extern uint SendInput(uint nInputs,[MarshalAs(UnmanagedType.LPArray)] INPUT[] pInputs, int cbSize);
-		[StructLayout(LayoutKind.Sequential)]
-		private struct KEYBOARD_INPUT
-		{
-			public ushort wVk;
-			public ushort wScan;
-			public uint dwFlags;
-			public uint time;
-			public IntPtr dwExtraInfo;
-		}
-		[StructLayout(LayoutKind.Explicit, Size = 28)]
-		private struct INPUT
-		{
-			[FieldOffset(0)]
-			public INPUT_TYPE type;
-			[FieldOffset(4)]
-			public KEYBOARD_INPUT ki;
-		}
-		private enum INPUT_TYPE : uint
-		{
-			MOUSE = 0,
-			KEYBOARD = 1,
-			HARDWARE = 2
-		}
-		private enum KEYEVENTF : uint
-		{
-			EXTENDEDKEY = 0x0001,
-			KEYUP = 0x0002,
-			UNICODE = 0x0004,
-			SCANCODE = 0x0008
-		}
-		private enum VK : ushort
-		{
-			//
-			// Virtual Keys, Standard Set
-			//
-			LBUTTON = 0x01,
-			RBUTTON = 0x02,
-			CANCEL = 0x03,
-			MBUTTON = 0x04,    // NOT contiguous with L & RBUTTON
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
 
-			XBUTTON1 = 0x05,    // NOT contiguous with L & RBUTTON
-			XBUTTON2 = 0x06,    // NOT contiguous with L & RBUTTON
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
 
-			// 0x07 : unassigned
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
 
-			BACK = 0x08,
-			TAB = 0x09,
+        struct HARDWAREINPUT
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
 
-			// 0x0A - 0x0B : reserved
+        [StructLayout(LayoutKind.Explicit)]
+        struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
 
-			CLEAR = 0x0C,
-			RETURN = 0x0D,
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
 
-			SHIFT = 0x10,
-			CONTROL = 0x11,
-			MENU = 0x12,
-			PAUSE = 0x13,
-			CAPITAL = 0x14,
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
 
-			KANA = 0x15,
-			HANGEUL = 0x15,  // old name - should be here for compatibility
-			HANGUL = 0x15,
-			JUNJA = 0x17,
-			FINAL = 0x18,
-			HANJA = 0x19,
-			KANJI = 0x19,
+        [StructLayout(LayoutKind.Sequential, Size = 28)]
+        struct INPUT
+        {
+            public int type;
+            public MOUSEKEYBDHARDWAREINPUT mkhi;
+        }
 
-			ESCAPE = 0x1B,
-
-			CONVERT = 0x1C,
-			NONCONVERT = 0x1D,
-			ACCEPT = 0x1E,
-			MODECHANGE = 0x1F,
-
-			SPACE = 0x20,
-			PRIOR = 0x21, // extended
-			NEXT = 0x22, // extended
-			END = 0x23, // extended
-			HOME = 0x24, // extended
-			LEFT = 0x25, // extended
-			UP = 0x26, // extended
-			RIGHT = 0x27, // extended
-			DOWN = 0x28, // extended
-			SELECT = 0x29, // extended
-			PRINT = 0x2A, // extended
-			EXECUTE = 0x2B, // extended
-			SNAPSHOT = 0x2C, // extended
-			INSERT = 0x2D, // extended
-			DELETE = 0x2E, // extended
-			HELP = 0x2F,
-
-			ZERO = 0x30,
-			ONE = 0x31,
-			TWO = 0x32,
-			THREE = 0x33,
-			FOUR = 0x34,
-			FIVE = 0x35,
-			SIX = 0x36,
-			SEVEN = 0x37,
-			EIGHT = 0x38,
-			NINE = 0x39,
-
-			// 0x40 : unassigned
-
-			A = 0x41,
-			B = 0x42,
-			C = 0x43,
-			D = 0x44,
-			E = 0x45,
-			F = 0x46,
-			G = 0x47,
-			H = 0x48,
-			I = 0x49,
-			J = 0x4A,
-			K = 0x4B,
-			L = 0x4C,
-			M = 0x4D,
-			N = 0x4E,
-			O = 0x4F,
-			P = 0x50,
-			Q = 0x51,
-			R = 0x52,
-			S = 0x53,
-			T = 0x54,
-			U = 0x55,
-			V = 0x56,
-			W = 0x57,
-			X = 0x58,
-			Y = 0x59,
-			Z = 0x5A,
-
-			LWIN = 0x5B, // extended
-			RWIN = 0x5C, // extended
-			APPS = 0x5D, // extended
-
-			//
-			// 0x5E : reserved
-			//
-
-			SLEEP = 0x5F,
-
-			NUMPAD0 = 0x60,
-			NUMPAD1 = 0x61,
-			NUMPAD2 = 0x62,
-			NUMPAD3 = 0x63,
-			NUMPAD4 = 0x64,
-			NUMPAD5 = 0x65,
-			NUMPAD6 = 0x66,
-			NUMPAD7 = 0x67,
-			NUMPAD8 = 0x68,
-			NUMPAD9 = 0x69,
-			MULTIPLY = 0x6A,
-			ADD = 0x6B,
-			SEPARATOR = 0x6C,
-			SUBTRACT = 0x6D,
-			DECIMAL = 0x6E,
-			DIVIDE = 0x6F,
-			F1 = 0x70,
-			F2 = 0x71,
-			F3 = 0x72,
-			F4 = 0x73,
-			F5 = 0x74,
-			F6 = 0x75,
-			F7 = 0x76,
-			F8 = 0x77,
-			F9 = 0x78,
-			F10 = 0x79,
-			F11 = 0x7A,
-			F12 = 0x7B,
-			F13 = 0x7C,
-			F14 = 0x7D,
-			F15 = 0x7E,
-			F16 = 0x7F,
-			F17 = 0x80,
-			F18 = 0x81,
-			F19 = 0x82,
-			F20 = 0x83,
-			F21 = 0x84,
-			F22 = 0x85,
-			F23 = 0x86,
-			F24 = 0x87,
-
-			//
-			// 0x88 - 0x8F : unassigned
-			//
-
-			NUMLOCK = 0x90,
-			SCROLL = 0x91,
-
-			//
-			// L* & R* - left and right Alt, Ctrl and Shift virtual keys.
-			// Used only as parameters to GetAsyncKeyState() and GetKeyState().
-			// No other API or message will distinguish left and right keys in this way.
-			//
-			LSHIFT = 0xA0,
-			RSHIFT = 0xA1,
-			LCONTROL = 0xA2,
-			RCONTROL = 0xA3,
-			LMENU = 0xA4,
-			RMENU = 0xA5,
-
-			BROWSER_BACK = 0xA6,
-			BROWSER_FORWARD = 0xA7,
-			BROWSER_REFRESH = 0xA8,
-			BROWSER_STOP = 0xA9,
-			BROWSER_SEARCH = 0xAA,
-			BROWSER_FAVORITES = 0xAB,
-			BROWSER_HOME = 0xAC,
-
-			VOLUME_MUTE = 0xAD,
-			VOLUME_DOWN = 0xAE,
-			VOLUME_UP = 0xAF,
-			MEDIA_NEXT_TRACK = 0xB0,
-			MEDIA_PREV_TRACK = 0xB1,
-			MEDIA_STOP = 0xB2,
-			MEDIA_PLAY_PAUSE = 0xB3,
-			LAUNCH_MAIL = 0xB4,
-			LAUNCH_MEDIA_SELECT = 0xB5,
-			LAUNCH_APP1 = 0xB6,
-			LAUNCH_APP2 = 0xB7,
-
-			//
-			// 0xB8 - 0xB9 : reserved
-			//
-
-			OEM_1 = 0xBA,   // ';:' for US
-			OEM_PLUS = 0xBB,   // '+' any country
-			OEM_COMMA = 0xBC,   // ',' any country
-			OEM_MINUS = 0xBD,   // '-' any country
-			OEM_PERIOD = 0xBE,   // '.' any country
-			OEM_2 = 0xBF,   // '/?' for US
-			OEM_3 = 0xC0,   // '`~' for US
-
-			//
-			// 0xC1 - 0xD7 : reserved
-			//
-
-			//
-			// 0xD8 - 0xDA : unassigned
-			//
-
-			OEM_4 = 0xDB,  //  '[{' for US
-			OEM_5 = 0xDC,  //  '\|' for US
-			OEM_6 = 0xDD,  //  ']}' for US
-			OEM_7 = 0xDE,  //  ''"' for US
-			OEM_8 = 0xDF
-
-			//
-			// 0xE0 : reserved
-			//
-		}
-	}
+        private const int INPUT_MOUSE = 0;
+        private const int INPUT_KEYBOARD = 1;
+        private const int INPUT_HARDWARE = 2;
+        private const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+        private const uint KEYEVENTF_KEYUP = 0x0002;
+        private const uint KEYEVENTF_UNICODE = 0x0004;
+        private const uint KEYEVENTF_SCANCODE = 0x0008;
+    }
 }
